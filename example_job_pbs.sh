eaa58e3e037d9fd69a3aa37cd421e63db0e44ced
Cankun Wang <flykun0620@gmail.com>
2020-12-02 11:22:00 -0500

    Add example scripts

---
+++ example_job_pbs.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+#PBS --N example_pbs_job
+#PBS -A PAS1475
+#PBS -l walltime==01:00:00
+#PBS -l nodes=1:ppn=8:gpus=1
+#PBS -l mem=8GB
+
+
+module load python/3.6
+cd $HOME
+pwd
+echo 'Running PBS jobs'
