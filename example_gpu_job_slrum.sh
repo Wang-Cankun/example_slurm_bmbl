af8d2fad7a5af19ad02d351ebb0ace4b29999909
Cankun Wang <flykun0620@gmail.com>
2021-02-18 01:03:21 -0500

    Update GPU usage

--- example_gpu_job_slrum.sh
+++ example_gpu_job_slrum.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 #SBATCH --job-name example_slurm_job
-#SBATCH --account PAS1475
+#SBATCH --account PCON0022
 #SBATCH --time=01:00:00
 #SBATCH --nodes=1 
 #SBATCH --ntasks=8
@@ -9,7 +9,9 @@
 
 date
 module load python/3.6
+ml cuda
 cd $HOME
 pwd
 echo 'Running slrum jobs - using GPU'
+nvidia-smi
 date
